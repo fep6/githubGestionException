4f1343516289a7c417829ebb5def8c8ea48d3ced
fep <franck.en.planck@gmail.com>
2019-06-20 11:52:05 +0200

    Erreurs multiples...

---
+++ GestionException/src/ConfigReader/Main.java
@@ -0,0 +1,29 @@
+import java.io.InputStream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ConfigReader() throws Exception {
+			Properties properties = new Properties();
+			Main leMain = new Main();
+			boolean superUser = leMain.isSuperUser();
+			InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties");
+			properties.load(input);
+
+			try {
+			cases = Integer.valueOf(properties.getProperty("NbCase", "4"));
+			colors = Integer.valueOf(properties.getProperty("NbCouleurSelec", "5"));
+			trials = Integer.valueOf(properties.getProperty("NbEssai", "10"));
+			if (superUser) developerMode = true;
+			else {
+			developerMode = Boolean.valueOf(properties.getProperty("developerMode", "false"));
+			}
+			} catch (NumberFormatException e) {
+			System.out.println("Error while reading configuration.");
+			}
+			}
+
+	}
+
+}
--- GestionException/src/PackageGestionException/Main.java
+++ GestionException/src/PackageGestionException/Main.java
@@ -1,10 +1,20 @@
 package PackageGestionException;
 
 public class Main {
-
+	// https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/22107-gerez-les-exceptions#/id/r-2181836
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i=0 , j=20;
+		
+		try {
+			System.out.println(" =>" +(j/i));
+		} catch (ArithmeticException e){
+			System.out.println("Division par zéro!  " + e.getMessage());
+		} finally {
+			System.out.println("Action faite systématiquement");
+		}
 
 	}
 
 }
+//https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/22107-gerez-les-exceptions#/id/r-2181880
